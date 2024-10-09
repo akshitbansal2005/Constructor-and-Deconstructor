5faba4195e6e5708e1eb5bfc06a771a0f6ca1d80
Akshit Bansal <155195875+akshitbansal2005@users.noreply.github.com>
2024-10-09 11:28:07 +0530

    Create Deconstructors.cpp

---
+++ Deconstructors.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int count=0;
+class Student{
+    public:
+    Student()
+    {
+        count++;
+        cout<<"number of objects created:"<<count<<endl;
+    }
+    ~Student()
+    {
+        count--;
+        cout<<"no.of objects destroyed:"<<count<<endl;
+    }
+};
+int main(){
+    Student aa,bb,cc;
+    {
+        Student dd;
+    }
+
+    return 0;
+}
+//Output
+/*number of objects created:1
+number of objects created:2
+number of objects created:3
+number of objects created:4
+no.of objects destroyed:3
+no.of objects destroyed:2
+no.of objects destroyed:1
+no.of objects destroyed:0
+*/
